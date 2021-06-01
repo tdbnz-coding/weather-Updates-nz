acd6e2e9335726e7f5a34148039a1def90335150
tdbnz-coding <49893160+tdbnz-coding@users.noreply.github.com>
2021-06-01 12:49:25 +1200

    Create refreshimages.js

---
+++ scripts/refreshimages.js
@@ -0,0 +1,12 @@
+<script>
+
+window.onload = function() {
+    var image = document.getElementById("img");
+
+    function updateImage() {
+        image.src = image.src.split("?")[0] + "?" + new Date().getTime();
+    }
+
+    setInterval(updateImage, 100);
+}
+</script>
